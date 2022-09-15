490f8095e4bdeeedec06714b63b60dfebca8a916
des-yogi <nuevo-design@mail.ru>
2022-09-15 13:47:22 +0300

    Required attr set&delete fix; Agreement script fix

--- src/blocks/event-modal/event-modal.js
+++ src/blocks/event-modal/event-modal.js
@@ -1,15 +1,35 @@
 (function(){
   const hiddenBlock = document.getElementById('collapsedBlock');
   const inviteTogglers = document.querySelectorAll('.visibilityTrigger');
+  if (!hiddenBlock) return;
+  const hiddenFields = hiddenBlock.querySelectorAll('.hidden-field');
 
   if (!inviteTogglers) return;
+
+  const setRequired = function () {
+    Array.prototype.forEach.call(hiddenFields, function (item) {
+      let field = item;
+      if (!field) return;
+      field.setAttribute('required', '');
+    });
+  };
+
+  const removeRequired = function () {
+    Array.prototype.forEach.call(hiddenFields, function (item) {
+      let field = item;
+      if (!field) return;
+      field.removeAttribute('required');
+    });
+  };
 
   const togglerHandler = function (e) {
     let radio = e.target;
     if (radio.value === '1' && radio.checked) {
       hiddenBlock.classList.add('show');
+      setRequired();
     } else {
       hiddenBlock.classList.remove('show');
+      removeRequired();
     }
   };
 

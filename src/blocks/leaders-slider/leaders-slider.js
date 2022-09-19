aef2f9df77aeec743f72e1820a96614efde1630e
des-yogi <nuevo-design@mail.ru>
2022-09-19 18:50:12 +0300

    slider leaders - done; pagination - done

--- src/blocks/leaders-slider/leaders-slider.js
+++ src/blocks/leaders-slider/leaders-slider.js
@@ -1,3 +1,12 @@
 (function(){
+  const leadersSlider = document.querySelector('.leaders-slider');
+  if (!leadersSlider) return;
 
+  const flkty = new Flickity( leadersSlider, {
+    cellAlign: 'left',
+    adaptiveHeight: false,
+    imagesLoaded: true,
+    contain: true,
+    pageDots: false
+  });
 }());
